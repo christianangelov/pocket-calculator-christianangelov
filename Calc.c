116f677eaa129563fd922bb34e65b18b722aef76
christianangelov <31957091+christianangelov@users.noreply.github.com>
2017-09-29 13:20:04 +0200

    Add files via upload

---
+++ Calc.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+
+  int Opperaton = menu();
+  double[] Opperators = GetOpperators();
+  double number = performOpperation(Opperaton, Opperators)
+
+
+  return 0;
+}
+
+int menu()
+{
+  printf("Choose Opperaton: Add (1) Subtract(2) Multiply(3) Divide(4) End Program (-1)\n");
+  int input = scanf("%d\n", );
+  return input;
+}
+double performOpperation(int Opperaton, double[] Opperators)
+{
+  double result;
+  switch (Opperaton) {
+    case 1:
+      break;
+    case 2:
+      break;
+    case 3:
+      break;
+    case 4:
+      break;
+    case 5:
+      break;
+    default:
+      break;
+  }
+  r
+}
+double[] GetOpperators()
+{
+  printf("First Opperant : ", );
+  double firstOpperant = scanf("%f\n", );
+  printf("Second Opperant : ", );
+  double secondOpperant = scanf("%f\n", );
+  
+  return firstOpperant;
+}
+int Add(int argc, char const *argv[]) {
+}
